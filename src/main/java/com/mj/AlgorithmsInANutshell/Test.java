ceffa86b5e56d0ce9dd97bece4745ff2c7acbf65
wangyt <wyt13322>
2018-09-19 11:48:47 +0800

    例子

---
+++ src/main/java/com/mj/AlgorithmsInANutshell/Test.java
@@ -0,0 +1,11 @@
+package com.mj.AlgorithmsInANutshell;
+
+/**
+ * @author TonyOne
+ * @version 1.0
+ * @desc
+ * @date 2018/9/19 10:58
+ * @company
+ */
+public class Test {
+}
---
+++ src/main/java/com/mj/DataStructrueAlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.mj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructrueAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructrueAlgorithmApplication.class, args);
+	}
+}
